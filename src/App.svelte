3d076f2e057d810415d2418fe3d025985323f9e2
Andrey E. Vedishchev <aevedis@protonmail.com>
2022-06-05 18:13:31 +0200

    First commit.

---
+++ src/App.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import Message from "./components/Message.svelte";
+
+	let show = true;
+	const handleToggle = () => {
+		show = !show;
+	}
+</script>
+
+<main>
+
+	{#if show}
+		<Message />
+	{:else}
+		console.log("")
+	{/if}
+	<button on:click={handleToggle}>Hidden/Shown</button>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		background-color: azure;
+		color: black;
+	}
+	button {
+		background-color: blueviolet;
+		border-radius: 1.5em;
+		color: white;
+		border: none;
+		padding: 10px;
+		margin: 10px;
+	}
+</style>
